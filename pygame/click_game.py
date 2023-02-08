ce8003bd7d6f57080a6e68eec83cb1d42ad04cd5
GavinAD <113545415+GavinAD@users.noreply.github.com>
2023-02-08 13:49:22 -0500

    Add files via upload

---
+++ pygame/click_game.py
@@ -0,0 +1,24 @@
+figure = Actor("character")
+figure.pos = 0, 0
+
+WIDTH = 500
+HEIGHT = 300
+BACKGROUND_COLOR = (174, 174, 174)
+
+def draw():
+    screen.fill(BACKGROUND_COLOR)
+    figure.draw()
+
+def update():
+    figure.left = figure.left + 2
+    if figure.left > WIDTH:
+        figure.right = 0
+    figure.bottom = figure.bottom + 1
+    if figure.bottom > HEIGHT:
+        figure.top = 0
+
+def on_mouse_down(pos):
+    if figure.collidepoint(pos):
+        print("Eek!)")
+    else:
+        print("You missed me!")
