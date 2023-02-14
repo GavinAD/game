f9ec8ec1130104d4597eee6080f9712dc3c55de4
GavinAD <113545415+GavinAD@users.noreply.github.com>
2023-02-14 13:59:44 -0500

    Update big-quiz.py

--- pygame/big-quiz/big-quiz.py
+++ pygame/big-quiz/big-quiz.py
@@ -21,31 +21,40 @@ score = 0
 
 time_left = 10
 
-q1 = ["What is the capital of France?",
-    "London", "Paris", "Berlin", "Tokyo", 2]
+q1 = ["When was the current flag of the United States adopted?",
+    "1773", "1775", "1625", "1960", 4]
 
-q2 = ["What is 5+7?",
-    "12", "10", "14", "8", 1]
+q2 = ["Which of these flags is an atypical shape?",
+    "Nepal", "China", "Mali", "Equador", 1]
 
-q3 = ["What is the seventh month of the year?",
-    "April", "May", "June", "July", 4]
+q3 = ["Which of these flags uses the pan-slavic colors?",
+    "Germany", "Poland", "Czechia", "Japan", 3]
 
-q4 = ["What planet is the closest to the Sun?",
-    "Saturn", "Neptune", "Mercury", "Venus", 3]
+q4 = ["Which flag features the most colors?",
+    "Russia", "Barbados", "Ghana", "Eswanti", 4]
 
-q5 = ["Where are the pyramids?", "India", "Egypt",
-    "Morocco", "Canada", 2]
+q5 = ["What is the oldest national flag still in use?", 
+    "China", "Denmark", "Morocco", "UK", 2]
+    
+q6 = ["Which of these flags uses the Nordic Cross?"
+    "Greenland", "Iceland", "England", "Germany", 2]
+    
+q7 = ["Which country's flag inspired a set of Pan-African colors?"
+    "Ethiopia", "Mali", "South Africa", "Egypt", 1]
+    
+q8 = ["What colors are on the flag of Angola?"
+    "Black, Green, Red", "Blue, Red, White", "Black, Blue, Yellow", "Black, Red, Yellow", 4]
 
-questions = [q1, q2, q3, q4, q5]
+questions = [q1, q2, q3, q4, q5, q6, q7, q8]
 question = questions.pop(0)
 
 def draw():
-    screen.fill("dim gray")
-    screen.draw.filled_rect(main_box, "sky blue")
-    screen.draw.filled_rect(timer_box, "sky blue")
+    screen.fill("dark red")
+    screen.draw.filled_rect(main_box, "indigo")
+    screen.draw.filled_rect(timer_box, "indigo")
 
     for box in answer_boxes:
-        screen.draw.filled_rect(box, "orange")
+        screen.draw.filled_rect(box, "purple")
 
     screen.draw.textbox(str(time_left), timer_box, color=("black"))
     screen.draw.textbox(question[0], main_box, color=("black"))
@@ -56,11 +65,39 @@ def draw():
         index = index + 1
 
 def game_over():
-    pass
+    global question, time_left
+    message = "Game Over, You Got %s Questions Correct" % str(score)
+    question = [message, "-", "-", "-", "-", 5]
+    time_left = 0
 
-def on_mouse_down():
-    pass
+def correct_answer():
+    global question, score, time_left
+    
+    score = score + 1
+    if questions:
+            question = questions.pop(0)
+            time_left = 10
+    else:
+        print("End of Questions")
+        game_over()
+
+def on_mouse_down(pos):
+    index = 1
+    for box in answer_boxes:
+        if box.collidepoint(pos):
+            print("Clicked on Answer " + str(index))
+            if index == question[5]:
+                print("You Got It Correct!")
+                correct_answer()
+            else:
+                game_over()
+        index = index + 1
 
 def update_timer_left():
-    pass
+    global time_left
+    
+    if time_left:
+        time_left = time_left - 1
+    else:
+        game_over()
 
